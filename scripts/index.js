1a43c3b077057ee69ae4d1d1d9f77da1d41a4d03
VladTolmachev <tolmachev368884@mail.ru>
2023-01-12 11:34:10 +0300

    fix: first fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,32 @@
 let popup = document.querySelector('.popup');
 let formElement = popup.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__profession');
-let submitButton = formElement.querySelector('.popup__submit');
+let popupForm = formElement.querySelector('.popup__form');
+let nameInput = popupForm.querySelector('[name="popup-name"]');
+let jobInput = popupForm.querySelector('[name="popup-profession"]');
 let closePopupButton = formElement.querySelector('.popup__btn-close');
 let profileProfession = document.querySelector('.profile__profession');
 let editButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
 
-nameInput.value = profileName.textContent;
-jobInput.value = profileProfession.textContent;
-
-
-editButton.addEventListener('click', function () {
+function openPopup () {
   popup.classList.add('popup_opened');
-})
 
-submitButton.addEventListener('click', function () {
-  function handleFormSubmit (evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileProfession.textContent = jobInput.value;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
 }
 
-  formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', openPopup)
+
+popupForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileProfession.textContent = jobInput.value;
 
   popup.classList.remove('popup_opened');
 })
 
-closePopupButton.addEventListener('click', function () {
+function closePopup () {
   popup.classList.remove('popup_opened');
-})
+}
+
+closePopupButton.addEventListener('click', closePopup);
